61e5cc161b9b0da7fff0541d81510be34c046cc7
BenjaminBalassa <balassa.benjamin@gmail.com>
2020-10-03 06:54:27 +0200

    pls work

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello HEROKU')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+
+app.get('/', (req, res) => {
+    res.json({visible: true})
+})
+
+app.listen(PORT, () => {console.log('Application started')})
